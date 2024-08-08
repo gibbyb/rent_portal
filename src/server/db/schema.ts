a575ccd3a14acb63a61eec97b48ceff53fc552e7
gibbyb <gib@gibbyb.com>
2024-08-08 04:49:14 -0500

    make sure I can push db as it is

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,4 +1,3 @@
-//import { sql } from "drizzle-orm";
 import {
   boolean,
   timestamp,
@@ -7,14 +6,15 @@ import {
   primaryKey,
   integer,
 } from "drizzle-orm/pg-core"
-import type { AdapterAccountType } from "next-auth/adapters"
 import postgres from "postgres"
 import { drizzle } from "drizzle-orm/postgres-js"
-
+import type { AdapterAccountType } from "next-auth/adapters"
+ 
 const connectionString = process.env.DATABASE_URL ?? "";
 const pool = postgres(connectionString, { max: 1 })
+ 
 export const db = drizzle(pool)
-
+ 
 export const users = pgTable("user", {
   id: text("id")
     .primaryKey()
